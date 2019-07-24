35dc2e4e79f5249e6b994759502ba8b6da267508
Curro Rodriguez <s1650043@ed.ac.uk>
2019-07-24 18:52:14 +0100

    Trial

--- tools.py
+++ tools.py
@@ -64,7 +64,7 @@ def mag_to_jansky(mag_AB):
 
     return f_nu
 
-def data_from_simba(ph_file, n_bands, magcols, mag_lim, ind_filt):
+def data_from_simba(ph_file, n_bands, mag_lim, ind_filt, ind_select):
     '''
     This routine reads in the selected apparent magnitudes from the SIMBA loser
     files in order to:
@@ -163,7 +163,7 @@ def superflux(minz, manz, dz, ind, wave, flux, flux_err, z, ll_eff):
 wave,spec,mean,var,ind,minz,maxz,dz,filternames,ll_eff = read_eigensystem('../VWSC_simba/EBASIS/VWSC_eigenbasis_0p5z3_wavemin2500.fits', '../VWSC_simba/FILTERS/vwsc_uds.lis')
 ind_filt = [0,1,2,3,4,5,6,7,8,11,12]
 n_bands = len(ll_eff)
-caesar_id, flux, flux_err, z, Kmag = data_from_simba('/home/rad/data/m100n1024/s50/Groups/phot_m100n1024_026.hdf5', n_bands, ind_filt,29.5,8)
+caesar_id, flux, flux_err, z, Kmag = data_from_simba('/home/rad/data/m100n1024/s50/Groups/phot_m100n1024_026.hdf5', n_bands, 29.5, ind_filt, 8)
 
 ll_obs = ll_eff[ind_filt]
 
