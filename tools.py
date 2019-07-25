d89aa0e3f64c4db9f3f94d428cf99ece3fc0354e
Curro Rodriguez <s1650043@ed.ac.uk>
2019-07-25 12:48:51 +0100

    Trial

--- tools.py
+++ tools.py
@@ -116,7 +116,6 @@ def fill_flux(flux, z, minz, maxz, dz, ll_obs, ind):
     nredshift = int((maxz-minz)/dz) + 1
     zbin = np.linspace(minz, maxz, nredshift)
     nz = len(zbin)
-    print(nz)
     n_band = len(ll_obs)
     ff = c_in_AA * flux / (ll_obs**2) # f_nu_obs to f_lambda_obs
     ff = ff * (1+z) # f_lambda_obs to f_lambda_rest
@@ -157,7 +156,216 @@ def superflux(minz, manz, dz, ind, wave, flux, flux_err, z, ll_eff):
 
     return flux_super, flux_super_err
 
+def normgappy(data, error, espec, mean, cov=False, reconstruct=False, verbose=False):
+    """
+    Performs robust PCA projection, including normalization estimation.
+    Parameters
+    ----------
+    data : ndarray
+        1D spectrum or 2D specta with 'float' type.
+    error : ndarray
+        1D or 2D corresponding 1-sigma error array. Zeros indicate masked data.
+    espec : ndarray
+        2D array of eigenspectra, possibly truncated in dimension.
+    mean : ndarray
+        1D mean spectrum of the eigenspectra.
+    cov: bool, optional
+        Return covariance matrix.
+        Default is ''False''.
+    reconstruct : bool, optional
+        Fill in missing values with PCA estimation.
+        Default is ''False''.
+    verbose : bool, optional
+        Enable for status and debug messages.
+        Default is ''False''
+    Returns
+    -------
+    pcs : ndrray
+        1D or 2D array of Principal Components with 'float' type.
+    norm: float or ndarray
+        Normalization estimates.
+    data : ndrray
+        If reconstruct enabled, 1D or 2D reconstructed spectra.
+    ccov : ndarray
+        If cov enabled, 2D or 3D covariance matrices.
+    """
 
+    # Sanity checks
+    if (np.size(data) == 0) | (np.size(error) == 0) | (np.size(espec) == 0) | (
+            np.size(mean) == 0):
+        print('[pca_normgappy] ERROR: incorrect input lengths')
+        return None
+
+    tmp = np.shape(espec)  # number of eigenvectors
+    if np.size(tmp) == 2:
+        nrecon = tmp[0]
+    else:
+        nrecon = 1
+    nbin = np.shape(espec)[-1]  # number of data points
+    tmp = np.shape(data)  # number of observations to project
+    if np.size(tmp) == 2:
+        ngal = tmp[0]
+    else:
+        ngal = 1
+
+    # Dimension mismatch check
+    if np.shape(data)[-1] != nbin:
+        print(
+            '[pca_normgappy] ERROR: "data" must have the same dimension as eigenvectors'
+        )
+        return None
+    if np.shape(error)[-1] != nbin:
+        print(
+            '[pca_normgappy] ERROR: "error" must have the same dimension as eigenvectors'
+        )
+        return None
+    if np.shape(mean)[0] != nbin:
+        print(
+            '[pca_normgappy] ERROR: "mean" must have the same dimension as eigenvectors'
+        )
+        return None
+
+    # Project each galaxy in turn
+    pcs = np.zeros((ngal, nrecon), float)
+    norm = np.zeros(ngal, float)
+    if cov is not None:
+        ccov = np.zeros((ngal, nrecon, nrecon))
+
+    if ngal == 1:
+        data = data[np.newaxis, :]
+        error = error[np.newaxis, :]
+
+    for j in np.arange(0, ngal):
+
+        if verbose:
+            print('[pca_normgappy] STATUS: processing spectrum ')
+
+        # Calculate weighting array from 1-sig error array
+        # ! if all bins have error=0 continue to next spectrum
+        weight = np.zeros(nbin)
+        ind = error[j, :].nonzero()[0]
+        if np.size(ind) != 0:
+            try:
+                weight[ind] = 1. / (error[j, :][ind]**2)
+            except:
+                if verbose:
+                    print(
+                        '[pca_normgappy] ERROR: error array problem in spectrum (setting pcs=0)'
+                    )
+                continue
+
+        ind = np.where(np.isfinite(weight) is False)[0]
+        if np.size(ind) != 0:
+            if verbose:
+                print(
+                    '[pca_normgappy] ERROR: error array problem in spectrum (setting pcs=0)'
+                )
+            continue
+
+        data_j = data[j, :]
+
+        # Solve partial chi^2/partial N = 0
+        Fpr = np.sum(weight * data_j * mean)  # eq 4 [2]
+        Mpr = np.sum(weight * mean * mean)  # eq 5 [2]
+        E = np.sum((weight * mean) * espec, axis=1)  # eq 6 [2]
+
+        # Calculate the weighted eigenvectors, multiplied by the eigenvectors (eq. 4-5 [1])
+
+        if nrecon > 1:
+            # CONSERVED MEMORY NOT IMPLEMETED
+            espec_big = np.repeat(espec[:, np.newaxis, :], nrecon, axis=1)
+            M = np.sum(weight * np.transpose(espec_big, (1, 0, 2)) * espec_big, 2)
+
+            # Calculate the weighted data array, multiplied by the eigenvectors (eq. 4-5 [1])
+            F = np.dot((data_j * weight), espec.T)
+
+            # Calculate new M matrix, this time accounting for the unknown normalization (eq. 11 [2])
+            E_big = np.repeat(E[np.newaxis, :], nrecon, axis=0)
+            F_big = np.repeat(F[:, np.newaxis], nrecon, axis=1)
+            Mnew = Fpr * M - E_big * F_big
+
+            # Calculate the new F matrix, accounting for unknown normalization
+            Fnew = Mpr * F - Fpr * E
+
+            # Solve for Principle Component Amplitudes (eq. 5 [1])
+            try:
+                Minv = np.linalg.inv(Mnew)
+            except:
+                if verbose:
+                    print(
+                        '[pca_normgappy] STATUS: problem with matrix inversion (setting pcs=0)'
+                    )
+
+                continue
+
+            pcs[j, :] = np.squeeze(np.sum(Fnew * Minv, 1))
+            norm[j] = Fpr / (Mpr + np.sum(pcs[j, :] * E))
+
+            # Calculate covariance matrix (eq. 6 [1])
+            if cov is True:
+                M_gappy = np.dot((espec * (weight * norm[j]**2)), espec.T)
+                ccov[j, :, :] = np.linalg.inv(M_gappy)
+
+        else:  # if only one eigenvector
+            M = np.sum(weight * espec * espec)
+            F = np.sum(weight * data_j * espec)
+            Mnew = M * Fpr - E * F
+            Fnew = Mpr * F - E * Fpr
+            pcs[j, 0] = Fnew / Mnew
+            norm[j] = Fpr / (Mpr + pcs[j, 0] * E)
+            if cov is True:
+                ccov[j, 0, 0] = np.sum((1. / weight) * espec * espec)
+
+        # If reconstruction of data array required,
+        #   fill in regions with weight = 0 with PCA reconstruction
+        if reconstruct is True:
+            bad_pix = np.where(weight == 0.)
+            count = np.size(bad_pix)
+            if count == 0:
+                continue
+
+            rreconstruct = np.sum((pcs[j, :] * espec[:, bad_pix].T).T, 0)
+            rreconstruct += mean[bad_pix]
+            data[j, bad_pix] = reconstruct
+
+    if ngal == 1:
+        pcs = pcs[0]
+        data = data[0]
+        norm = norm[0]
+        if cov:
+            ccov = ccov[0]
+
+    # Report to user
+    if verbose:
+        print("[pca_normgappy] STATUS: Results...")
+        for i, pc in enumerate(pcs):
+            print(f"               PCA{i+1}: {pc:2.5f}")
+        print(f"               Norm: {norm:2.5f}")
+
+    # Return
+    if reconstruct is True:
+        if cov is True:
+            return pcs, norm, data, ccov
+        else:
+            return pcs, norm, data
+
+    elif cov is True:
+        return pcs, norm, ccov
+    else:
+        return pcs, norm
+
+def SC1_vs_SC2_scatter(pc_data):
+
+    x = pc_data[:,0] #SC1
+    y = pc_data[:,1] #SC2
+    fig = plt.figure(num=None, figsize=(6, 6), dpi=80, facecolor='w', edgecolor='k')
+    ax = fig.add_subplot(1,1,1)
+    ax.set_ylabel('SC 2', fontsize=16)
+    ax.set_xlabel('SC 1', fontsize=16)
+    ax.scatter(x,y)
+    fig.tight_layout()
+    fig.savefig('../color_plots/sc1_vs_sc2.png',
+                    format='png', dpi=250, bbox_inches='tight')
 wave,spec,mean,var,ind,minz,maxz,dz,filternames,ll_eff = read_eigensystem('../VWSC_simba/EBASIS/VWSC_eigenbasis_0p5z3_wavemin2500.fits', '../VWSC_simba/FILTERS/vwsc_uds.lis')
 ind_filt = [0,1,2,3,4,5,6,7,8,11,12]
 n_bands = len(ll_eff)
@@ -167,5 +375,6 @@ ll_obs = ll_eff[ind_filt]
 
 flux_super, flux_super_err = superflux(minz, maxz, dz, ind, wave, flux, flux_err, z, ll_eff)
 
-print(flux_err)
-print(flux_super_err)
+pcs = normgappy(flux_super,flux_super_err,spec,mean)
+
+SC1_vs_SC2_scatter(pcs)
