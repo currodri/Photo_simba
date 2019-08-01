aea2d611739725dfc38c038307fcce562f6d3256
Curro Rodriguez <s1650043@ed.ac.uk>
2019-08-01 14:20:12 +0100

    Check

--- loser_data.py
+++ loser_data.py
@@ -238,9 +238,9 @@ def uvj_quench(redshift,caesar_id,Labs,sfr,mstar,quench_file):
                         elif galaxy.galaxy_t[end] <= t_hubble and not galaxy.rate:
                             #possible_q.append(galaxy.galaxy_t[end])
                             #possible_tau.append(quench.quench_time/galaxy.galaxy_t[end])
-                            if np.log10(sfr[i]/(10**mstar[i])+1e-14) >= sfr_condition_2('end',t_hubble):
-                                print(np.log10(sfr[i]/(10**mstar[i])+1e-14), galaxy.id)
-                            else:
+                            if np.log10(sfr[i]/(10**mstar[i])+1e-14) >= sfr_condition_2('start',t_hubble):
+                                print(np.log10(sfr[i]/(10**mstar[i])+1e-14), galaxy.id, caesar_id)
+                            elif np.log10(sfr[i]/(10**mstar[i])+1e-14) < sfr_condition_2('end',t_hubble):
                                 possible_q.append(galaxy.galaxy_t[end])
                                 possible_tau.append(quench.quench_time/galaxy.galaxy_t[end])
         if possible_q:
