3c45b36a15cb91f8b1c7c64d71737e6ae7188acc
Curro Rodriguez <s1650043@ed.ac.uk>
2019-08-09 13:23:26 +0100

    New UVJ

--- uvj_plots.py
+++ uvj_plots.py
@@ -90,7 +90,7 @@ def uvj_quench(redshift,galaxies,masslimit):
                         sSFR.append(ssfr)
                         q_time.append(snap_t-gal.t[0][indx])
                         tau_q.append(quench.quench_time)
-            elif np.log10(m_gal[pos2]) >= masslimit:
+            elif np.log10(m_gal[0][pos2]) >= masslimit:
                 U_non.append(gal.mags[0].Abs[gal.mags[0].z==redshift])
                 V_non.append(gal.mags[1].Abs[gal.mags[1].z==redshift])
                 J_non.append(gal.mags[2].Abs[gal.mags[2].z==redshift])
