897b0ca4ddd85855f8abe429860c1f93e03e6fcc
Curro Rodriguez Montero <s1650043@cplab223.ph.ed.ac.uk>
2019-08-16 10:41:48 +0100

    Test old

--- tools.py
+++ tools.py
@@ -507,7 +507,7 @@ def SC1_vs_SC2_scatter(pc_data,snap):
     ax = fig.add_subplot(1,1,1)
     ax.set_ylabel('SC 2', fontsize=16)
     ax.set_xlabel('SC 1', fontsize=16)
-    ax.scatter(x,y)
+    ax.scatter(x,y, s=10)
     #ax.set_xlim([-50,150])
     #ax.set_ylim([-20, 30])
     fig.tight_layout()
@@ -517,7 +517,9 @@ def SC1_vs_SC2_scatter(pc_data,snap):
 wave,spec,mean,var,ind,minz,maxz,dz,filternames,ll_eff = read_eigensystem('../VWSC_simba/EBASIS/VWSC_eigenbasis_0p5z3_wavemin2500.fits', '../VWSC_simba/FILTERS/vwsc_uds.lis')
 ind_filt = [0,1,2,3,4,5,6,7,8,11,12]
 n_bands = len(ll_eff)
+print(n_bands)
 caesar_id, flux, flux_err, z, Kmag = data_from_pyloser('../VWSC_simba/CATS/simba/loserpsb_m50n512_125.hdf5', n_bands, 24.5, ind_filt, 8)
+print(flux[0],flux_err[0])
 
 ll_obs = ll_eff[ind_filt]
 
